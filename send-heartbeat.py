2375ae54e1215cd224baef2e7649a6dd2f74bc38
Krzysztof Rakowski <krzysztof@rakowski.pl>
2013-01-27 00:49:37 +0100

    papertrail port moved to the env var

--- send-heartbeat.py
+++ send-heartbeat.py
@@ -1,10 +1,11 @@
 # based on http://help.papertrailapp.com/kb/configuration/configuring-centralized-logging-from-python-apps
-import logging
+# IMPORTANT NOTE: remeember to set PAPERTRAIL_PORT enviroment variable
+import logging, os
 
 from logging.handlers import SysLogHandler
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
-syslog = SysLogHandler(address=('logs.papertrailapp.com', 57814))
+syslog = SysLogHandler(address=('logs.papertrailapp.com', int(os.environ['PAPERTRAIL_PORT'])))
 formatter = logging.Formatter('%(name)s: %(levelname)s %(message)s')
 syslog.setFormatter(formatter)
 logger.addHandler(syslog)
